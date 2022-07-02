e04e56ceac41c7785f5a6ca10aafb6e5781e2dd1
Semaff <semarc-zhu@yandex.ru>
2022-07-02 21:00:30 +0300

    Included All Slices

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,13 +3,19 @@ import productsReducer from "./productsSlice";
 import categoriesReducer from "./categoriesSlice";
 import userReducer from "./userSlice";
 import testimonialsReducer from "./testimonialsSlice";
+import basketReducer from "./basketSlice";
+import orderReducer from "./orderSlice";
+import brandsReducer from "./brandsSlice";
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         products: productsReducer,
         categories: categoriesReducer,
-        testimonials: testimonialsReducer
+        brands: brandsReducer,
+        testimonials: testimonialsReducer,
+        basket: basketReducer,
+        order: orderReducer,
     }
 });
 
