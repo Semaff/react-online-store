116013136454639e73535f2362d970dad2405793
Semaff <semarc-zhu@yandex.ru>
2022-07-02 21:04:19 +0300

    Added functionality to Cart page

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Table, Timeline, Total } from '../../containers';
+import { addedBasketCoupon, removedBasketCoupon, selectBasketCoupon, selectBasketProducts } from '../../store/basketSlice';
 import "./Cart.scss"
 
+const checkCoupon = (coupon) => {
+    switch (coupon) {
+        case "FIRST TIME": {
+            return { discount: 5, status: true };
+        }
+        default: {
+            return { status: false };
+        }
+    }
+}
+
 const Cart = () => {
+    const couponRef = useRef();
+    const dispatch = useDispatch();
+    let couponActivated = false;
+
+    const basketProducts = useSelector(selectBasketProducts);
+    const basketCoupon = useSelector(selectBasketCoupon)
+
+    const handleCouponButttonClick = () => {
+        couponActivated = checkCoupon(couponRef.current.value);
+        if (couponActivated.status) {
+            dispatch(addedBasketCoupon(couponActivated.discount))
+        } else {
+            dispatch(removedBasketCoupon(couponActivated.discount))
+        }
+    }
+
+    const total = basketProducts.reduce((acc, item) => acc += item.price * item.basket_product.quantity, 0);
+
     return (
         <>
             <Timeline page="Cart" />
@@ -10,8 +41,8 @@ const Cart = () => {
             <section className="section  --fullPadding">
                 <div className="container">
                     <div className="cart">
-                        <Table />
-                        <Total />
+                        <Table products={basketProducts} couponRef={couponRef} onCouponClick={handleCouponButttonClick} />
+                        <Total coupon={basketCoupon} total={total} />
                     </div>
                 </div>
             </section>
