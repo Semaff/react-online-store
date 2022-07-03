9cb7883e11cdcd29a262adf031ad34fc5ec4a029
Semaff <semarc-zhu@yandex.ru>
2022-07-03 12:26:46 +0300

    Deleted useless HTML

--- client/src/containers/Middlebar/Middlebar.jsx
+++ client/src/containers/Middlebar/Middlebar.jsx
@@ -15,22 +15,6 @@ const Middlebar = () => {
 
                 <div className="middlebar__inner">
                     <span className="middlebar__left">
-                        <div className="middlebar__select">
-                            <select name="currency" id="currency">
-                                <option value="USD">USD</option>
-                                <option value="RUB">RUB</option>
-                            </select>
-                            <span className="middlebar__select-focus"></span>
-                        </div>
-
-                        <div className="middlebar__select">
-                            <select name="language" id="language">
-                                <option value="English">English</option>
-                                <option value="Russian">Russian</option>
-                            </select>
-                            <span className="middlebar__select-focus"></span>
-                        </div>
-
                         <div className="middlebar__phone">
                             <Phone />
                             <a href="tel:+095587-55680">095587-55680</a>
@@ -44,7 +28,6 @@ const Middlebar = () => {
                     </span>
 
                     <span className="middlebar__right">
-                        <Search />
                         <Link to={!isLoggedIn ? SIGNIN_ROUTE : PROFILE_ROUTE}>
                             <Profile />
                         </Link>
