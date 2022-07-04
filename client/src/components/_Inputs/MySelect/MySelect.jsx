0584473b491be59f130885267b969df65e0b8db6
Semaff <semarc-zhu@yandex.ru>
2022-07-04 22:59:16 +0300

    Fixed big with controlled/uncontrolled select

--- client/src/components/_Inputs/MySelect/MySelect.jsx
+++ client/src/components/_Inputs/MySelect/MySelect.jsx
@@ -1,6 +1,6 @@
 import "./MySelect.scss";
 
-const MySelect = ({ name, options, labelText, isImportant, defaultValue, value, onChange }) => {
+const MySelect = ({ name, options, labelText, isImportant, value, defaultValue, onChange }) => {
     return (
         <div className="input__placeholder">
             {labelText && (
@@ -11,12 +11,13 @@ const MySelect = ({ name, options, labelText, isImportant, defaultValue, value, 
 
             <select
                 className="select"
+                value={value || undefined}
+                defaultValue={defaultValue || undefined}
                 name={name}
                 id={name}
-                value={value}
                 onChange={onChange}
             >
-                <option value="" disabled>{defaultValue || "Choose..."}</option>
+                <option value="default" disabled>Choose...</option>
 
                 {options && options.length > 0 && options.map(option => (
                     <option value={option} key={option}>{option}</option>
