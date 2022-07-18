c7a8a7dd351ec7ac86a88339e8908eaf93a928fc
Semaff <semarc-zhu@yandex.ru>
2022-07-18 22:45:28 +0300

    Fixed bug with always changing DB

--- server/db.js
+++ server/db.js
@@ -1,11 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     dialectOptions: {
         ssl: {
